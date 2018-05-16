90e81af241bc848e77637f2f57400fccf802330e
Theo M Jones <theomjones@gmail.com>
2018-05-16 11:42:40 +0100

    postcss-modules

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import styles from "./app.css";
+console.log(styles);
+
 const App = props => (
-  <div>
+  <div className={styles.App}>
     <h1>{props.title}</h1>
   </div>
 );
 
-ReactDOM.render(<App title={"Hello world."} />, document.getElementById("app"));
+ReactDOM.render(
+  <App title={"React starter"} />,
+  document.getElementById("app")
+);
